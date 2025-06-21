42bb5e885731b4a8f5aa62493abb73334e8e3ee4
NIRAV SATASHIA <khushbusatasiya@gmail.com>
2025-06-21 07:37:56 +0100

    adding my files

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import App from './App';
+import AdminHome from './adminhome';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let id=localStorage.getItem("userid"); 
+//fetching data from browser memory but it is not exists so it return null. let id=null; let id=123
+
+if(id===null){
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}else{
+  root.render(
+    <React.StrictMode>
+      <AdminHome />
+    </React.StrictMode>
+  );
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
